fc0cf80270afbfa775b07eaea5a7468ece18b502
Ryusei Nagai <ryuuryuu908@gmail.com>
2021-02-20 23:22:48 +0900

    click logo then go back

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,13 +1,34 @@
 import React from "react";
+import { useHistory, useLocation, useEffect } from "react-router-dom";
 
-export default class Header extends React.Component {
-    render() {
-        return (
-            <header>
-                <div className="header-container">
-                    <img className="logo" src="../img/logo.png" />
-                </div>
-            </header>
-        );
+// export default class Header extends React.Component {
+//     render() {
+//         return (
+//             <header>
+//                 <div className="header-container">
+//                     <img className="logo" src="../img/logo.png" />
+//                 </div>
+//             </header>
+//         );
+//     }
+// }
+
+export default function Header(props) {
+    const history = useHistory();
+    const location = useLocation();
+
+    const handleClick = () => {
+        history.goBack();
     }
+    return (
+        <header>
+            <div className="header-container">
+                {location.pathname === "/setting" ? 
+                    <img className="logo" id="setting-header" src="../img/logo.png" onClick={handleClick}/>
+                    :
+                    <img className="logo" src="../img/logo.png" ></img>
+                }
+            </div>
+        </header>
+    );
 }
