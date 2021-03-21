fcd59dd2269ebe6ef23d69acf4abdf8d21f6a6f8
Ryusei Nagai <ryuuryuu908@gmail.com>
2021-03-21 15:28:08 +0900

    separate pages from components

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,10 +1,8 @@
 import React, { useEffect } from "react";
-import Login from "./components/Login";
-import User from "./components/User";
-import Setting from "./components/Setting"
 import { BrowserRouter as Router, Route} from "react-router-dom";
-import Cookies from 'js-cookie';
 import LoginPage from "./pages/LoginPage";
+import UserPage from "./pages/UserPage";
+import SettingPage from "./pages/SettingPage";
 
 
 export default function Layout() {
@@ -13,8 +11,8 @@ export default function Layout() {
         <Router>
             <div className="router">
             <Route exact path="/" component={LoginPage} />
-            <Route path="/user/:token" component={User} />
-            <Route path="/setting" component={Setting} />
+            <Route path="/user/:token" component={UserPage} />
+            <Route path="/setting" component={SettingPage} />
             </div>
         </Router>
         </div>
